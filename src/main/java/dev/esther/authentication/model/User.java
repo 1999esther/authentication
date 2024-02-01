ede8ba412a7ebbecaed0089b15779d407c5fb2d1
1999esther <99013648+1999esther@users.noreply.github.com>
2024-02-01 10:26:34 +0300

    Here sofar there is Validating the JWT(Json web token) in the JwtService and JwtAuthenticationFilter.

--- src/main/java/dev/esther/authentication/model/User.java
+++ src/main/java/dev/esther/authentication/model/User.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 
 @Data
@@ -25,37 +27,43 @@ public class User implements UserDetails {
     private String firstname;
     private String lastname;
     private String password;
+    private String email;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
         return null;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
